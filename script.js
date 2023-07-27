3941500c9fe65ef1569361fd43ba12c509891726
Arialyson Lima <arialyson@gmail.com>
2023-07-27 14:47:55 -0300

    Trocar avatar
    Troca da imagem de perfil ao mudar o tema com Javascript

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ function togglemode() {
     } else {
         html.classList.add('light')
     }
+
+    /* Ou pode usar somente o comando simplificado:
+
+    html.classList.toggle("light")
+
+    */
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    //Substituir a imagem
+    if(html.classList.contains("light")) {
+    //Se tiver light mode, adicionar a imagem light
+        img.setAttribute("src","assets/avatar-light.png")
+}   else {
+    //se tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "assets/avatar.png")
 }
-/* Ou pode usar somente o comando simplificado:
 
-html.classList.toggle("light")
-
-*/
+}
